dc0f8fe81c1574bc3cc2b1f96785d0f028791bbd
Akshaj-Goyal2012 <akshaj.goyal0812@gmail.com>
2026-07-04 08:04:24 +0000

    Changes in P2.py

--- LEV3/P2.py
+++ LEV3/P2.py
@@ -1,5 +1,5 @@
 import csv
 import random
-with open ('fc25_top_100_by_position.csv', mode='r') as file:
+with open ('fc25_top_100_no_rank_by_position.csv', mode='r') as file:
     csvFile=csv.DictReader(file)
     CARDS=list(csvFile)
