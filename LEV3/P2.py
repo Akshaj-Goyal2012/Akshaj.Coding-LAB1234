00006ed4a1033fd8daf5f0989bb7ede6ecd14aff
Akshaj-Goyal2012 <akshaj.goyal0812@gmail.com>
2026-07-09 15:17:24 +0000

    Changes in P2.py

--- LEV3/P2.py
+++ LEV3/P2.py
@@ -3,3 +3,25 @@ import random
 with open ('fc25_top_100_no_rank_by_position.csv', mode='r') as file:
     csvFile=csv.DictReader(file)
     CARDS=list(csvFile)
+
+#Intro
+
+print('Welcome to Baller Clash.')
+print('This is a game where you need fight with Computer and win the Card.')
+print('Click enter to begin.')
+
+input()
+
+
+def display_card(card):
+  # Display the attributes of a card
+  # A card is a dictionary with several attributes
+
+  max_chars = 0
+  
+  for keys in card:
+    if len(keys) > max_chars:
+      max_chars = len(keys)
+  
+  for keys in card:
+    print(keys, (max_chars-len(keys))*' ', ': ', card[keys])
