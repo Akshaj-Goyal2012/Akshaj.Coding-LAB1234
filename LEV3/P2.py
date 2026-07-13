8930b12a8d35787c90e5d132d2b725b35d1573a0
Akshaj-Goyal2012 <akshaj.goyal0812@gmail.com>
2026-07-13 16:10:20 +0000

    Changes in code of P2.py

--- LEV3/P2.py
+++ LEV3/P2.py
@@ -1,8 +1,8 @@
 import csv
 import random
-with open ('fc25_top_100_no_rank_by_position.csv', mode='r') as file:
+with open ('LEV3/fc25_top_100_no_rank_by_position.csv', mode='r') as file:
     csvFile=csv.DictReader(file)
-    CARDS=list(csvFile)
+    all_cards=list(csvFile)
 
 #Intro
 
@@ -13,15 +13,38 @@ print('Click enter to begin.')
 input()
 
 
-def display_card(card):
-  # Display the attributes of a card
-  # A card is a dictionary with several attributes
+Releventkeys=list(all_cards[0].keys())
+Releventkeys=Releventkeys[3::]
 
-  max_chars = 0
-  
-  for keys in card:
-    if len(keys) > max_chars:
-      max_chars = len(keys)
-  
-  for keys in card:
-    print(keys, (max_chars-len(keys))*' ', ': ', card[keys])
+random.shuffle(all_cards)
+player_cards=all_cards[0::2]
+computer_cards=all_cards[1::2]
+table_cards=[]
+
+chance=random.randint(0,1)
+
+if chance ==0:
+    chance='Player'
+else:
+    chance='Computer'
+
+game = True
+
+while game:
+    print('Players Card', len(player_cards), 'Computer card',len(computer_cards),'Table Cards',len(table_cards))
+    player=player_cards.pop(0)
+    Computer=computer_cards.pop(0)
+
+    table_cards.append(player)
+    table_cards.append(Computer)
+
+    print()
+    
+    print('It is',chance, 'Chance now')
+    print()
+    print('The player cards are:')
+    
+
+
+ 
+ 
